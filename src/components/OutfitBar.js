8c2bcd8db13522bf484f82ce539c4700c4fc51ed
Mikael Risyad <mikelrisyad@gmail.com>
2022-03-24 21:48:30 +0700

    feat: add jumbotron component

--- src/components/OutfitBar.js
+++ src/components/OutfitBar.js
@@ -2,21 +2,72 @@ const OutfitBar = () => {
   return (
     <>
       <ul className="flex space-x-8 w-full bg-[#FFFFFF] ml-4 overflow-x-auto">
-        <li className="m-1">
-          New Arrivals <span className="bg-red-500">#Fresh</span>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800 ml-11">
+          <button>
+            <a>New Arrivals </a>
+          </button>
+          <span className="bg-red-500 p-1 text-white rounded ml-2">#Fresh</span>
         </li>
-        <li className="m-1">Dresses</li>
-        <li className="m-1">Tops</li>
-        <li className="m-1">Jackets</li>
-        <li className="m-1">Lingerie & Lounge Wear</li>
-        <li className="m-1">Beach Wear</li>
-        <li className="m-1">Blouse</li>
-        <li className="m-1">Denim</li>
-        <li className="m-1">Vintage</li>
-        <li className="m-1">Shoes</li>
-        <li className="m-1">Sandals</li>
-        <li className="m-1">Bags</li>
-        <li className="m-1">Jewelries</li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Dresses</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Tops</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Jackets</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Lingerie & Lounge Wear</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Beach Wear</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Blouse</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Denim</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Vintage</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Shoes</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Sandals</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Bags</a>
+          </button>
+        </li>
+        <li className="whitespace-nowrap mr-2 p-2 my-1 border-b-2 border-white hover:border-b-2 hover:border-gray-800">
+          <button>
+            <a>Jewelries</a>
+          </button>
+        </li>
       </ul>
     </>
   );
--- src/components/homepage-component/Jumbotron.js
+++ src/components/homepage-component/Jumbotron.js
@@ -1,8 +1,23 @@
 const Jumbotron = () => {
   return (
-    <div className="flex w-full h-full">
-      <div className="bg-black">image1</div>
-      <div className="bg-blue-200">image2</div>
+    <div className="flex  absolute left-0 w-full ">
+      <div className="bg-[#1C2333] text-white  flex w-full h-screen">
+        <div className="align-middle ml-[100px] my-auto rounded-lg justify-center p-11 max-w-[500px] bg-gray-700">
+          <h1 className="text-white text-5xl font-bold">
+            Get Up to 30% off{' '}
+            <span className="text-yellow-600">New Arrival</span>
+          </h1>
+          <h2 className="my-3 text-white text-xl">
+            Introducing our latest collection Product
+          </h2>
+          <button className="border p-3 mt-5 border-white">
+            <a>PLACE YOUR ORDER</a>
+          </button>
+        </div>
+      </div>
+      <div className="bg-[#F7C59F] flex w-full basis-4/6">
+        <div className="rounded-full m-auto bg-white w-[341px] h-[341px]"></div>
+      </div>
     </div>
   );
 };
