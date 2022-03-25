5bd9d61b349b1dbfd255459905f2907f23d41653
Mikael Risyad <mikelrisyad@gmail.com>
2022-03-25 15:51:12 +0700

    fix: change base url api to https

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://berita-indo-api.vercel.app';
+const baseUrl = 'https://berita-indo-api.vercel.app';
 
 export const getTribunNews = async () => {
   const response = await axios.get(`${baseUrl}/v1/tribun-news/`);
